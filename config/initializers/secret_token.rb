384b7dc4187be4747c4ac0b02e4a656fa0aae26d
Steven Occhipinti <steve.occhipinti@rea-group.com>
2013-06-20 22:49:49 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskbreakdown::Application.config.secret_token = '8b171f72c1f9996282e6d1331392a240cbe2589ba9e1240bef32807907d176e49453ec2851e9ad4a1b6bc9e6f831cebdf188893443921e2976a91fcf09a4f4f6'
