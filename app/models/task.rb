e7c091599bb3f6e566ec8426cf8d977d59df4d82
Steven Occhipinti <steve.occhipinti@rea-group.com>
2013-06-22 01:28:34 +1000

    Make subtasks work!

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ActiveRecord::Base
-  attr_accessible :description, :name, :percentage_complete
+  attr_accessible :description, :name, :percentage_complete, :parent_id
   has_ancestry
 end
