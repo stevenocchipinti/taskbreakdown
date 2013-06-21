61d87d6e84484ab6a4607ec99eb21c91ca2bc487
Steven Occhipinti <steve.occhipinti@rea-group.com>
2013-06-22 02:21:38 +1000

    Show percentage pills

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,5 +1,13 @@
 module TasksHelper
+
   def back_path(task)
     task.parent ? task_path(task.parent) : tasks_path
   end
+
+  def percentage_span_for(task)
+    content_tag :span,
+      "#{((task.percentage_complete||0)*100).to_i}%",
+      :class => task.percentage_complete == 1 ? "count-positive" : "count"
+  end
+
 end
