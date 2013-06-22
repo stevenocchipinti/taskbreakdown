1fdb2886c0fe448112cc665e72bd0e980e4b6e7d
Steven Occhipinti <steve.occhipinti@rea-group.com>
2013-06-22 18:24:15 +1000

    Calculate completion percentage for subtrees

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,25 @@
 class Task < ActiveRecord::Base
   attr_accessible :description, :name, :percentage_complete, :parent_id
   has_ancestry
+
+  def percentage_complete
+    if has_children?
+      calculate_completion subtree.arrange
+    else
+      read_attribute :percentage_complete
+    end
+  end
+
+  private
+
+  def calculate_completion(subtree)
+    subtree.inject(0) do |a, (task,subtasks)|
+      if subtasks.size > 0
+        a += calculate_completion(subtasks)
+      else
+        a += task.read_attribute(:percentage_complete)
+      end
+    end / subtree.size
+  end
+
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,7 +4,7 @@ describe Task do
   describe "#percentage_complete" do
     context "when subtasks exist" do
 
-      subject do
+      let(:task_with_subtasks) do
         root = FactoryGirl.create :task, name: "root", percentage_complete: 0.50
         t1 = FactoryGirl.create :task,
           name: "root",
@@ -18,7 +18,45 @@ describe Task do
         root
       end
 
-      it { pending }
+      subject { task.percentage_complete }
+
+      context "when the task DOES NOT have any subtasks" do
+        let(:task) { FactoryGirl.create :task, percentage_complete: 0.50 }
+        it { should == 0.50 }
+      end
+
+      context "when the task DOES have any subtasks" do
+        #         * [ 32.5% ]
+        #       /   \
+        #     /       \
+        #    * 20%     * [ 45% ]   1%
+        #            /   \
+        #          /       \
+        #         * 15%     * 75%
+        let(:task) do
+          root = FactoryGirl.create :task, name: "root", percentage_complete: 0.01
+          t1 = FactoryGirl.create :task,
+            name: "Subtask1",
+            percentage_complete: 0.20,
+            parent: root
+          t2 = FactoryGirl.create :task,
+            name: "Subtask2",
+            percentage_complete: 0.01,
+            parent: root
+          t3 = FactoryGirl.create :task,
+            name: "Subtask3",
+            percentage_complete: 0.15,
+            parent: t2
+          t4 = FactoryGirl.create :task,
+            name: "Subtask4",
+            percentage_complete: 0.75,
+            parent: t2
+          root.save
+          root
+        end
+
+        it { should == 0.325 }
+      end
 
     end
   end
