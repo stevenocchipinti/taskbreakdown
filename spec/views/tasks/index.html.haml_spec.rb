b34134f3f1244847b000153b9ccdb931af533bb4
Steven Occhipinti <steve.occhipinti@rea-group.com>
2013-06-22 11:08:18 +1000

    Fix up scaffolded specs

--- spec/views/tasks/index.html.haml_spec.rb
+++ spec/views/tasks/index.html.haml_spec.rb
@@ -6,21 +6,19 @@ describe "tasks/index" do
       stub_model(Task,
         :name => "Name",
         :description => "MyText",
-        :percentage_complete => "9.99"
+        :percentage_complete => "0.50"
       ),
       stub_model(Task,
         :name => "Name",
         :description => "MyText",
-        :percentage_complete => "9.99"
+        :percentage_complete => "0.50"
       )
     ])
   end
 
   it "renders a list of tasks" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
+    assert_select "li strong", :text => "Name".to_s, :count => 2
+    assert_select "li span", :text => "50%".to_s, :count => 2
   end
 end
--- spec/views/tasks/show.html.haml_spec.rb
+++ spec/views/tasks/show.html.haml_spec.rb
@@ -5,7 +5,7 @@ describe "tasks/show" do
     @task = assign(:task, stub_model(Task,
       :name => "Name",
       :description => "MyText",
-      :percentage_complete => "9.99"
+      :percentage_complete => "0.5"
     ))
   end
 
@@ -14,6 +14,6 @@ describe "tasks/show" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Name/)
     rendered.should match(/MyText/)
-    rendered.should match(/9.99/)
+    rendered.should match(/50/)
   end
 end
