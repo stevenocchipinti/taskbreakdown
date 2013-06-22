9cb758bbc041bcdef7c43e8ad5f74b5a0279eea4
Steven Occhipinti <steve.occhipinti@rea-group.com>
2013-06-22 13:44:16 +1000

    Use FactoryGirl to create nested tasks

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,9 +1,26 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+
   factory :task do
-    name "MyString"
-    description "MyText"
-    percentage_complete "9.99"
+    name "A generic task"
+    description "I'm just like everyone else"
+    percentage_complete 0.4
+
+    # This sub-factory can only be created, not built because of Ancestry
+    factory :task_with_subtasks, :class => Task do
+      name "Root task"
+      description "I have no parents"
+      percentage_complete 0.4
+      ignore { tasks_count 5 }
+      ignore { subtasks_percentage_complete 0.1 }
+      after(:create) do |task, evaluator|
+        FactoryGirl.create_list :task, evaluator.tasks_count, {
+          percentage_complete: evaluator.subtasks_percentage_complete,
+          parent: task
+        }
+      end
+    end
   end
+
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,4 +1,25 @@
 require 'spec_helper'
 
 describe Task do
+  describe "#percentage_complete" do
+    context "when subtasks exist" do
+
+      subject do
+        root = FactoryGirl.create :task, name: "root", percentage_complete: 0.50
+        t1 = FactoryGirl.create :task,
+          name: "root",
+          percentage_complete: 0.10,
+          parent: root
+        t2 = FactoryGirl.create :task,
+          name: "root",
+          percentage_complete: 0.10,
+          parent: root
+        root.save
+        root
+      end
+
+      it { pending }
+
+    end
+  end
 end
